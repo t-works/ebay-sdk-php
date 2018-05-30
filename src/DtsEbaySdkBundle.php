464609f3e7fdeba6a9e076fb8b1eb2edd1f3c237
jerzy <jerzykurowski@gmail.com>
2018-05-30 04:06:59 +0200

    added bundle

---
+++ src/DtsEbaySdkBundle.php
@@ -0,0 +1,20 @@
+<?php
+namespace DTS\eBaySDK;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ *
+ *
+ */
+class DtsEbaySdkBundle extends Bundle
+{
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+
+}
